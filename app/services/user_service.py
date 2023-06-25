bcc4ffd612b96729d6cda4096c326c7f601bf542
kleberjr <kleb3r.jr@gmail.com>
2023-06-25 17:25:15 -0300

    fix: password updated without hash

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -10,16 +10,11 @@ from app.database.schemas import UserUpdate as UserUpdateSchema
 import bcrypt
 
 def create(db: Session, user: UserCreateSchema):
-    hashed_password = bcrypt.hashpw(
-        user.password.encode('utf-8'),
-        bcrypt.gensalt()
-    )
-    
     db_user = UserModel(
         college_id=user.college_id,
         name=user.name,
         email=f"{user.college_id}@aluno.unb.br",
-        password=hashed_password,
+        password=get_hashed_password(user.password),
         is_active=False,
         balance=0.00
     )
@@ -37,11 +32,9 @@ def get_by_email(db: Session, email: str):
     return db.query(UserModel).filter(UserModel.email == email).first()
 
 def update(db: Session, user: UserUpdateSchema, college_id: str):
-    # TODO: hash password before saving
-
     db.query(UserModel).filter(UserModel.college_id == college_id).update({
         'name': user.name,
-        'password': user.password,
+        'password': get_hashed_password(user.password),
         'is_active': user.is_active,
         'balance': user.balance
     })
@@ -52,14 +45,6 @@ def delete(db: Session, college_id: str):
     db.query(AccessModel).filter(AccessModel.user_college_id == college_id).delete()
     db.query(UserModel).filter(UserModel.college_id == college_id).delete()
     db.commit()
-
-def auth_user(db: Session, user: UserBaseSchema):
-    query_user = get(db=db, college_id=user.college_id)
-    
-    if not query_user or not bcrypt.checkpw(user.password.encode('utf-8'), query_user.password.encode('utf-8')):
-        raise Exception
-    
-    return query_user
 
 def activate(db: Session, college_id: str):
     db.query(UserModel).filter(UserModel.college_id == college_id).update({
@@ -74,3 +59,9 @@ def deactivate(db: Session, college_id: str):
     })
 
     db.commit()
+
+def get_hashed_password(password: str):
+    return bcrypt.hashpw(
+        password.encode('utf-8'),
+        bcrypt.gensalt()
+    )  
